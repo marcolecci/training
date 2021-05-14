0f2aedba766f04fa167a544b04d00b3b8b8096fa
marcolecci <marco.lecci@monash.edu>
2021-05-14 10:55:08 +1000

    Return None, not 0, on invalid input

--- calc.py
+++ calc.py
@@ -1,10 +1,10 @@
-def compute(expression):
-    values = expression.split(' ')
-    num0 = int(values[0])
-    operator = values[1]
-    num1 = int(values[2])
-    if operator == '+':
-        return num0 + num1
-    else:
-        print('unknown operator!')
-        return 0
+def compudef compute(expression):
+    valuevalues = expression.split(' ')
+    num0 num0 = int(values[0])
+    operaoperator = values[1]
+    num1 num1 = int(values[2])
+    if opif operator == '+':
+        rreturn num0 + num1
+    else:else:
+    else:print('unknown operator!')
+        rreturn 0
